930ae7d1608793aeb0df0b4b93d17952c0adcefd
lang <mla@mailbox.org>
2020-12-02 14:10:38 +0100

    testing cached eval function

--- src/test/java/org/mattlang/jc/perftests/LegalMoveCacheTest.java
+++ src/test/java/org/mattlang/jc/perftests/LegalMoveCacheTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import org.mattlang.jc.Factory;
 import org.mattlang.jc.board.Board;
 import org.mattlang.jc.engine.BasicMoveList;
+import org.mattlang.jc.engine.evaluation.SimpleNegaMaxEval;
 import org.mattlang.jc.movegenerator.MoveGenerator;
 import org.mattlang.jc.engine.MoveList;
 import org.mattlang.jc.engine.compactmovelist.CompactMoveList;
@@ -17,6 +18,9 @@ import org.mattlang.jc.engine.compactmovelist.CompactMoveList;
 @Ignore
 public class LegalMoveCacheTest  {
 
+    /**
+     * REsult: currently the compactmovelist ist slower than the basicmovelist (thats really surprising...)
+     */
     @Test
     public void compareit() {
         Board board = new Board();
@@ -56,8 +60,12 @@ public class LegalMoveCacheTest  {
 
     }
 
+    /**
+     * Caching movelists with board as key: this is much faster than recreating, therefore very beneficial
+     * for the engine search algorithms.
+     */
     @Test
-    public void compareHashCache() {
+    public void compareMoveListHashCache() {
         Board board = new Board();
 
         board.setStartPosition();
@@ -102,5 +110,53 @@ public class LegalMoveCacheTest  {
         System.out.println("use hashmap: " + stopwatch.toString());
 
     }
+
+    /**
+     * caching eval function result: in classical mini/max caching does not help, since  the eval function
+     * is only used on the leafs and therefore not multiple times.
+     * In incremental analysis however it could be beneficial but we need also there testing to ensure it really.
+     */
+    @Test
+    public void compareEvalFunctionHashCache() {
+        Board board = new Board();
+
+        board.setStartPosition();
+
+        System.out.println(board.toUniCodeStr());
+
+        StopWatch stopwatch = new StopWatch();
+
+        Factory.setMoveListSupplier(() -> new BasicMoveList());
+
+        SimpleNegaMaxEval eval = new SimpleNegaMaxEval();
+
+        stopwatch.start();
+        int num = 100_000_000;
+        for (int i = 0; i < num; i++) {
+            eval.eval(board, WHITE);
+            eval.eval(board, BLACK);
+        }
+        stopwatch.stop();
+        System.out.println("always evaluate: " + stopwatch.toString());
+
+        stopwatch.start();
+
+        HashMap<Board, Integer> whitemap = new HashMap<>();
+        HashMap<Board, Integer> blackmap = new HashMap<>();
+
+        whitemap.put(board,  eval.eval(board, WHITE));
+        blackmap.put(board, eval.eval(board, BLACK));
+
+
+        for (int i = 0; i < num; i++) {
+            whitemap.get(board);
+            blackmap.get(board);
+
+        }
+        stopwatch.stop();
+        System.out.println("use hashmap: " + stopwatch.toString());
+
+    }
+
 
 }
