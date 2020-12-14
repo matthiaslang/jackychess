ae2c4f81f4cecac183330caaa559deae6ae70587
Matthias Lang <mla@mailbox.org>
2020-12-14 14:23:15 +0100

    code cleanup

--- src/main/java/org/mattlang/jc/movegenerator/CachingLegalMoveGenerator.java
+++ src/main/java/org/mattlang/jc/movegenerator/CachingLegalMoveGenerator.java
@@ -1,15 +1,14 @@
 package org.mattlang.jc.movegenerator;
 
-import org.mattlang.jc.Factory;
 import org.mattlang.jc.board.Board;
 import org.mattlang.jc.board.Color;
-import org.mattlang.jc.board.Figure;
-import org.mattlang.jc.engine.MoveCursor;
 import org.mattlang.jc.engine.MoveList;
 
 public class CachingLegalMoveGenerator implements LegalMoveGenerator {
 
     private BoardCache<MoveList> legalMoveCache = new BoardCache<>(this::generateLegalMoves);
+
+    private LegalMoveGenerator delegate = new LegalMoveGeneratorImpl();
 
     @Override
     public MoveList generate(Board board, Color side) {
@@ -17,30 +16,6 @@ public class CachingLegalMoveGenerator implements LegalMoveGenerator {
     }
 
     private MoveList generateLegalMoves(Board board, Color side) {
-        MoveGenerator generator = Factory.createMoveGenerator();
-        MoveList moves = generator.generate(board, side);
-        MoveList legalMoves = filterLegalMoves(board, moves, side == Color.WHITE ? Color.BLACK : Color.WHITE);
-        // simple ordering by capture first, to be a bit bettr in alpha beta pruning
-        legalMoves.sortByCapture();
-        return legalMoves;
+        return delegate.generate(board, side);
     }
-
-    private MoveList filterLegalMoves(Board currBoard, MoveList moves, Color color) {
-        Figure king = color == Color.WHITE ? Figure.B_King : Figure.W_King;
-
-        MoveList legals = Factory.createMoveList();
-        for (MoveCursor moveCursor : moves) {
-            moveCursor.move(currBoard);
-            MoveGenerator generator = Factory.createMoveGenerator();
-            MoveList responseMoves = generator.generate(currBoard, color);
-            moveCursor.undoMove(currBoard);
-
-            if (!responseMoves.capturesFigure(king)) {
-                legals.addMove(moveCursor);
-            }
-
-        }
-        return legals;
-    }
-
 }
