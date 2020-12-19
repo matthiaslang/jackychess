93bfded1139b9d76fbccfc16ff3abbc5299b4347
Matthias Lang <mla@mailbox.org>
2020-12-19 21:57:22 +0100

    game phase

--- src/main/java/org/mattlang/jc/engine/evaluation/GamePhase.java
+++ src/main/java/org/mattlang/jc/engine/evaluation/GamePhase.java
@@ -1,5 +1,9 @@
 package org.mattlang.jc.engine.evaluation;
 
+import org.mattlang.jc.board.Board;
+import org.mattlang.jc.board.BoardRepresentation;
+import org.mattlang.jc.board.FigureType;
+
 /**
  * Gamephase: Open, Middle or Endgame.
  */
@@ -7,5 +11,33 @@ public enum GamePhase {
 
     Opening,
     Middle,
-    End,
+    End;
+
+
+    public static GamePhase determineGamePhase(BoardRepresentation board) {
+
+        Board startBoard = new Board();
+        startBoard.setStartPosition();
+        int diffs = 0;
+        int figuresOnBoard = 0;
+
+        for (int i = 0; i < 64; i++) {
+            byte figureCode = board.getFigureCode(i);
+            if (figureCode != startBoard.getFigureCode(i)) {
+                diffs++;
+            }
+            if (figureCode != FigureType.EMPTY.figureCode) {
+                figuresOnBoard++;
+            }
+        }
+        if (figuresOnBoard < 14) {
+            return End;
+        }
+
+        if (diffs < 14) {
+            return Opening;
+        }
+
+        return Middle;
+    }
 }
