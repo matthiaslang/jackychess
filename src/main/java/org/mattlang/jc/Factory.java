39220d138375bfb7a7dc8fe19a91906f0fe421b1
Matthias Lang <mla@mailbox.org>
2020-12-27 22:33:25 +0100

    use opt material eval function

--- src/main/java/org/mattlang/jc/Factory.java
+++ src/main/java/org/mattlang/jc/Factory.java
@@ -1,8 +1,7 @@
 package org.mattlang.jc;
 
 import org.mattlang.jc.board.Board2;
-import org.mattlang.jc.engine.evaluation.CachingEvaluateFunction;
-import org.mattlang.jc.engine.evaluation.MaterialNegaMaxEval;
+import org.mattlang.jc.engine.evaluation.MaterialNegaMaxEvalOpt;
 import org.mattlang.jc.engine.search.IterativeDeepeningMtdf;
 import org.mattlang.jc.engine.search.IterativeDeepeningNegaMaxAlphaBeta;
 import org.mattlang.jc.movegenerator.LegalMoveGeneratorImpl3;
@@ -36,7 +35,7 @@ public class Factory {
 
     public static SearchParameter createIterativeDeepeningAlphaBeta() {
         return new SearchParameter()
-                .evaluateFunction.set(() -> new CachingEvaluateFunction(new MaterialNegaMaxEval()))
+                .evaluateFunction.set(() -> new MaterialNegaMaxEvalOpt())
                 .moveGenerator.set(() -> new MoveGeneratorImpl2())
                 .legalMoveGenerator.set(() -> new LegalMoveGeneratorImpl3())
                 .boards.set(() -> new Board2())
@@ -48,7 +47,7 @@ public class Factory {
 
     public static SearchParameter createIterativeDeepeningMtdf() {
         return new SearchParameter()
-                .evaluateFunction.set(() -> new CachingEvaluateFunction(new MaterialNegaMaxEval()))
+                .evaluateFunction.set(() -> new MaterialNegaMaxEvalOpt())
                 .moveGenerator.set(() -> new MoveGeneratorImpl2())
                 .legalMoveGenerator.set(() -> new LegalMoveGeneratorImpl3())
                 .setMaxDepth(6)
