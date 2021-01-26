e5d80d826a6ba728211fc33b6803074c41c4146c
Matthias Lang <mla@enowa.ag>
2021-01-26 11:27:57 +0100

    benchmark test to compare different experimental configurations

---
+++ src/test/java/org/mattlang/jc/engine/EngineBenchmarkTest.java
@@ -0,0 +1,61 @@
+package org.mattlang.jc.engine;
+
+import static org.mattlang.jc.Main.initLogging;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.mattlang.jc.Benchmarks;
+import org.mattlang.jc.Factory;
+import org.mattlang.jc.StopWatch;
+import org.mattlang.jc.board.Board2;
+import org.mattlang.jc.board.Move;
+import org.mattlang.jc.movegenerator.LegalMoveGeneratorImpl4;
+import org.mattlang.jc.uci.UCI;
+
+public class EngineBenchmarkTest {
+
+
+    @Test
+    public void benchmark() throws IOException {
+        initLogging();
+        UCI.instance.attachStreams();
+        
+        StopWatch watchNormal= Benchmarks.benchmark("Normal iterat deep",
+                () ->{
+                    Factory.setDefaults(Factory.createIterativeDeepeningAlphaBeta()
+                            .setTimeout(600000)
+                            .setMaxDepth(7));
+                    // now starting engine:
+                    Engine engine = new Engine(new Board2());
+
+                    engine.getBoard().setStartPosition();
+                    Move move = engine.go();
+
+                    System.out.println(move.toStr());
+
+                });
+
+        StopWatch watchOpt= Benchmarks.benchmark("optimized",
+                () ->{
+                    Factory.setDefaults(Factory.createIterativeDeepeningAlphaBeta()
+                            .setTimeout(600000)
+                            .legalMoveGenerator.set(LegalMoveGeneratorImpl4::new)
+
+                            .setMaxDepth(7));
+                    // now starting engine:
+                    Engine engine = new Engine(new Board2());
+
+                    engine.getBoard().setStartPosition();
+                    Move move = engine.go();
+
+                    System.out.println(move.toStr());
+
+                });
+
+        System.out.println("normal: "+ watchNormal.toString());
+        System.out.println("optimized: "+ watchOpt.toString());
+
+    }
+
+}
