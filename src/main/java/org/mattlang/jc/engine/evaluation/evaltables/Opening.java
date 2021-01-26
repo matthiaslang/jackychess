615d8cbd2d83b9611bd863acc406d57d2e96ab6b
Matthias Lang <mla@enowa.ag>
2021-01-26 16:26:01 +0100

    changed patterns a bit accourding to https://www.chessprogramming.org/Simplified_Evaluation_Function

--- src/main/java/org/mattlang/jc/engine/evaluation/evaltables/Opening.java
+++ src/main/java/org/mattlang/jc/engine/evaluation/evaltables/Opening.java
@@ -10,42 +10,53 @@ public class Opening {
      * Bishop pattern with bonus to develop to center places and penalty for start position
      */
     private final static Pattern BISHOP_PATTERN = new Pattern(new byte[] {
-            0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0,
-            0, 2, 3, 3, 3, 3, 2, 0,
-            0, 2, 5, 5, 5, 5, 2, 0,
-            0, 0, 3, 5, 5, 3, 0, 0,
-            0, 0, 0, 2, 2, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0,
+            -20,-10,-10,-10,-10,-10,-10,-20,
+            -10,  0,  0,  0,  0,  0,  0,-10,
+            -10,  0,  5, 10, 10,  5,  0,-10,
+            -10,  5,  5, 10, 10,  5,  5,-10,
+            -10,  0, 10, 10, 10, 10,  0,-10,
+            -10, 10, 10, 10, 10, 10, 10,-10,
+            -10,  5,  0,  0,  0,  0,  5,-10,
+            -20,-10,-10,-10,-10,-10,-10,-20,
     });
 
     /**
      * Knight pattern with bonus to develop to center places and penalty for start position
      */
     private final static Pattern KNIGHT_PATTERN = new Pattern(new byte[] {
-            0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0,
-            0, 1, 3, 3, 3, 3, 1, 0,
-            0, 2, 3, 5, 5, 3, 2, 0,
-            0, 0, 3, 5, 5, 3, 0, 0,
-            0, 0, 0, 2, 2, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0,
+            -50,-40,-30,-30,-30,-30,-40,-50,
+            -40,-20,  0,  0,  0,  0,-20,-40,
+            -30,  0, 10, 15, 15, 10,  0,-30,
+            -30,  5, 15, 20, 20, 15,  5,-30,
+            -30,  0, 15, 20, 20, 15,  0,-30,
+            -30,  5, 10, 15, 15, 10,  5,-30,
+            -40,-20,  0,  5,  5,  0,-20,-40,
+            -50,-40,-30,-30,-30,-30,-40,-50,
     });
 
     /**
      * Queen pattern with penalty to not develop to early...
      */
     private final static Pattern QUEEN_PATTERN = new Pattern(new byte[] {
-            -120, -120, -120, -120, -120, -120, -120, -120,
-            -120, -120, -120, -120, -120, -120, -120, -120,
-            -120, -120, -120, -120, -120, -120, -120, -120,
-            -120, -120, -60, -60, -60, -60, -120, -120,
-            -120, -120, -60, -60, -60, -60, -120, -120,
-            -120, -120, -60, -60, -60, -60, -120, -120,
-            -120, -120, -60, -60, -60, -60, -120, -120,
-            0, 0, 0, 0, 0, 0, 0, 0,
+            -20,-10,-10, -5, -5,-10,-10,-20,
+            -10,  0,  0,  0,  0,  0,  0,-10,
+            -10,  0,  5,  5,  5,  5,  0,-10,
+            -5,  0,  5,  5,  5,  5,  0, -5,
+            0,  0,  5,  5,  5,  5,  0, -5,
+            -10,  5,  5,  5,  5,  5,  0,-10,
+            -10,  0,  5,  0,  0,  0,  0,-10,
+            -20,-10,-10, -5, -5,-10,-10,-20
+    });
+
+    private final static Pattern KING_PATTERN = new Pattern(new byte[]{
+            -30,-40,-40,-50,-50,-40,-40,-30,
+            -30,-40,-40,-50,-50,-40,-40,-30,
+            -30,-40,-40,-50,-50,-40,-40,-30,
+            -30,-40,-40,-50,-50,-40,-40,-30,
+            -20,-30,-30,-40,-40,-30,-30,-20,
+            -10,-20,-20,-20,-20,-20,-20,-10,
+            20, 20,  0,  0,  0,  0, 20, 20,
+            20, 30, 10,  0,  0, 10, 30, 20
     });
 
     public static final int openingPatterns(BoardRepresentation currBoard, Color who2Move) {
@@ -58,7 +69,8 @@ public class Opening {
         score += BISHOP_PATTERN.calcScore(wp.getBishops(), bp.getBishops(), who2mov);
         score += KNIGHT_PATTERN.calcScore(wp.getKnights(), bp.getKnights(), who2mov);
         score += QUEEN_PATTERN.calcScore(wp.getQueens(), bp.getQueens(), who2mov);
-
+        score += KING_PATTERN.calcScore(wp.getKing(), bp.getKing(), who2mov);
+        
         return score;
     }
 
