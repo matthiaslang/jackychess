6cc443c8fd23e27a7c1cffb0e9c7a73a6a031f95
Matthias Lang <mla@enowa.ag>
2021-02-14 19:29:23 +0100

    default quiescence of 10

--- src/main/java/org/mattlang/jc/ConfigValues.java
+++ src/main/java/org/mattlang/jc/ConfigValues.java
@@ -10,7 +10,7 @@ public class ConfigValues {
     private Map<String, UCIOption> allOptions = new LinkedHashMap<>();
 
     public final UCISpinOption timeout = new UCITimeoutOption(allOptions);
-    public final UCISpinOption maxQuiescence = new UCISpinOption(allOptions, "quiescence", 0, 10, 0);
+    public final UCISpinOption maxQuiescence = new UCISpinOption(allOptions, "quiescence", 0, 10, 10);
     public final UCISpinOption maxDepth = new UCISpinOption(allOptions, "maxdepth", 3, 30, 15);
 
     public final UCICheckOption activatePvsSearch = new UCICheckOption(allOptions, "activatePvsSearch", true);
