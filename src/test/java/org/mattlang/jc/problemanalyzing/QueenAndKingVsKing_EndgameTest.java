0e2df0e6f7f4d4e610c7c6b51c34b1746b1e8e50
Matthias Lang <mla@enowa.ag>
2021-06-07 15:15:48 +0200

    first test case to analyze current end game problems

---
+++ src/test/java/org/mattlang/jc/problemanalyzing/QueenAndKingVsKing_EndgameTest.java
@@ -0,0 +1,54 @@
+package org.mattlang.jc.problemanalyzing;
+
+import static org.mattlang.jc.Main.initLogging;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.mattlang.jc.Factory;
+import org.mattlang.jc.board.GameState;
+import org.mattlang.jc.board.Move;
+import org.mattlang.jc.engine.Engine;
+import org.mattlang.jc.engine.SearchMethod;
+import org.mattlang.jc.engine.search.IterativeDeepeningPVS;
+import org.mattlang.jc.engine.search.IterativeSearchResult;
+import org.mattlang.jc.uci.GameContext;
+import org.mattlang.jc.uci.UCI;
+
+public class QueenAndKingVsKing_EndgameTest {
+
+    int maxDepth = 12;
+
+    @Test
+    public void queen_and_king_vs_king() throws IOException {
+
+        initLogging();
+        UCI.instance.attachStreams();
+        Factory.setDefaults(Factory.createDefaultParameter()
+                .config(c -> c.maxDepth.setValue(maxDepth))
+                .config(c -> c.useTTCache.setValue(true))
+//                .config(c -> c.evluateFunctions.setValue(MINIMAL_PST))
+                .config(c -> c.timeout.setValue(600)));
+        // now starting engine:
+        Engine engine = new Engine();
+        GameState gameState = engine.getBoard().setFenPosition("position fen 8/8/4k3/8/3Q4/4K3/8/8 w - - 0 0 ");
+        System.out.println(engine.getBoard().toUniCodeStr());
+
+        SearchMethod searchMethod = Factory.getDefaults().searchMethod.create();
+
+        IterativeDeepeningPVS itDeep = (IterativeDeepeningPVS) searchMethod;
+        IterativeSearchResult itResult =
+                itDeep.iterativeSearch(gameState, new GameContext(), maxDepth);
+
+        // execute moves on board of pv:
+        for (Move move : itResult.getRslt().pvList.getPvMoves()) {
+            gameState.getBoard().move(move);
+
+            System.out.println(move.toStr());
+            System.out.println(gameState.getBoard().toUniCodeStr());
+        }
+
+        Factory.setDefaults(Factory.createDefaultParameter());
+    }
+
+}
