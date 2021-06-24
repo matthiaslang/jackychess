a85269c7205a5b4b49a7540dd5117ab0e924a6a8
Matthias Lang <mla@enowa.ag>
2021-06-24 13:30:45 +0200

    using lazy listin pv list

--- src/main/java/org/mattlang/jc/engine/search/PVList.java
+++ src/main/java/org/mattlang/jc/engine/search/PVList.java
@@ -7,45 +7,54 @@ import java.util.List;
 
 import org.mattlang.jc.board.Move;
 
-public class PVList {
+public final class PVList {
 
-    private ArrayList<Move> list = new ArrayList<>();
+    private ArrayList<Move> list = null;
 
     public PVList() {
 
     }
 
     public PVList(ArrayList<Move> pvs) {
-        list.addAll(pvs);
+        getLazyList().addAll(pvs);
     }
 
     public final void clear() {
-        list.clear();
+        list = null;
     }
 
     public final void add(PVList otherList) {
-        list.addAll(otherList.list);
+        if (otherList.list != null) {
+            getLazyList().addAll(otherList.getLazyList());
+        }
     }
 
     public void set(Move move) {
-        list.clear();
-        list.add(move);
+        list = null;
+        getLazyList().add(move);
     }
 
     public Move get(int index) {
-        if (index < list.size()) {
-            return list.get(index);
+        if (index < getLazyList().size()) {
+            return getLazyList().get(index);
         }
         return null;
     }
 
     public String toPvStr() {
-        return list.stream()
+        return getLazyList().stream()
                 .map(m -> m.toStr())
                 .collect(joining(" "));
     }
 
     public List<Move> getPvMoves() {
-        return new ArrayList<>(list);
+        return new ArrayList<>(getLazyList());
+    }
+
+    private List<Move> getLazyList() {
+        if (list == null) {
+            list = new ArrayList<>();
+        }
+        return list;
     }
 }
