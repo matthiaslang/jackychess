f160d1a0245abf49a2e02b141c330ff5c54febad
lang <mla@mailbox.org>
2021-08-11 17:01:52 +0200

    fix: evaluation functions do not check anymore for patt. this is done in negamax itself

--- src/main/java/org/mattlang/jc/engine/evaluation/DefaultEvaluateFunction.java
+++ src/main/java/org/mattlang/jc/engine/evaluation/DefaultEvaluateFunction.java
@@ -1,7 +1,5 @@
 package org.mattlang.jc.engine.evaluation;
 
-import static org.mattlang.jc.engine.evaluation.Weights.PATT_WEIGHT;
-
 import org.mattlang.jc.Factory;
 import org.mattlang.jc.board.BoardRepresentation;
 import org.mattlang.jc.board.Color;
@@ -31,20 +29,15 @@ public class DefaultEvaluateFunction implements EvaluateFunction {
         BoardStats wstats = statsgenerator.gen(currBoard, Color.WHITE);
         BoardStats bstats = statsgenerator.gen(currBoard, Color.BLACK);
 
-        int pattCheck = PattChecking.pattCheck(wstats, bstats, who2Move);
-        if (pattCheck == -PATT_WEIGHT || pattCheck == PATT_WEIGHT) {
-            return pattCheck;
-        }
-
         int score = commonEval.eval(currBoard, wstats, bstats, who2Move);
 
         EvalStats evalStats = new EvalStats(currBoard);
 
-        score+=pawnStructureEval.eval(evalStats, who2Move);
+        score += pawnStructureEval.eval(evalStats, who2Move);
 
-        score+=endGameMaterialEval.eval(evalStats, who2Move);
+        score += endGameMaterialEval.eval(evalStats, who2Move);
 
-        score+=kingSafetyEval.eval(currBoard, evalStats, who2Move);
+        score += kingSafetyEval.eval(currBoard, evalStats, who2Move);
 
         return score;
     }
--- src/main/java/org/mattlang/jc/engine/evaluation/taperedEval/GamePhaseEvaluation.java
+++ src/main/java/org/mattlang/jc/engine/evaluation/taperedEval/GamePhaseEvaluation.java
@@ -1,14 +1,11 @@
 package org.mattlang.jc.engine.evaluation.taperedEval;
 
-import static org.mattlang.jc.engine.evaluation.Weights.PATT_WEIGHT;
-
 import org.mattlang.jc.Factory;
 import org.mattlang.jc.board.BoardRepresentation;
 import org.mattlang.jc.board.Color;
 import org.mattlang.jc.engine.EvaluateFunction;
 import org.mattlang.jc.engine.evaluation.BoardStats;
 import org.mattlang.jc.engine.evaluation.BoardStatsGenerator;
-import org.mattlang.jc.engine.evaluation.PattChecking;
 
 /**
  * Tapered Evaluation function with special opening and endgame evaluate function.
@@ -28,12 +25,6 @@ public class GamePhaseEvaluation implements EvaluateFunction {
 
         BoardStats wstats = statsgenerator.gen(currBoard, Color.WHITE);
         BoardStats bstats = statsgenerator.gen(currBoard, Color.BLACK);
-
-        int pattCheck = PattChecking.pattCheck(wstats, bstats, who2Move);
-        if (pattCheck == -PATT_WEIGHT || pattCheck == PATT_WEIGHT) {
-            return pattCheck;
-        }
-
 
         int common = commonEval.eval(currBoard, wstats, bstats, who2Move);
 
