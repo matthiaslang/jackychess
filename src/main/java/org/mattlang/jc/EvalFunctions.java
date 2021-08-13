1cd25a5029c74b57ef5a21c81d86d24340b2c069
lang <mla@mailbox.org>
2021-08-13 11:29:42 +0200

    removed silly tapered eval function

--- src/main/java/org/mattlang/jc/EvalFunctions.java
+++ src/main/java/org/mattlang/jc/EvalFunctions.java
@@ -6,7 +6,6 @@ import org.mattlang.jc.engine.EvaluateFunction;
 import org.mattlang.jc.engine.evaluation.DefaultEvaluateFunction;
 import org.mattlang.jc.engine.evaluation.minimalpst.MinimalPstEvaluation;
 import org.mattlang.jc.engine.evaluation.pstEval2.PstEvaluation2;
-import org.mattlang.jc.engine.evaluation.taperedEval.GamePhaseEvaluation;
 
 public enum EvalFunctions {
 
@@ -28,13 +27,6 @@ public enum EvalFunctions {
         @Override
         public Supplier<EvaluateFunction> createSupplier() {
             return PstEvaluation2::new;
-        }
-    },
-
-    TAPERED {
-        @Override
-        public Supplier<EvaluateFunction> createSupplier() {
-            return GamePhaseEvaluation::new;
         }
     };
 
