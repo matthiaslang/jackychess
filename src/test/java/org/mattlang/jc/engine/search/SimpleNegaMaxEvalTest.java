e61a4484e3465989a8103cb72b69ee01d3aa4120
lang <mla@mailbox.org>
2021-08-27 17:25:30 +0200

    fixed Testcase: one test was obsolete, since the eval functions do not test for patt anymore; other test needs to attach streams to work properly

--- src/test/java/org/mattlang/jc/engine/search/SimpleNegaMaxEvalTest.java
+++ src/test/java/org/mattlang/jc/engine/search/SimpleNegaMaxEvalTest.java
@@ -1,43 +1,30 @@
 package org.mattlang.jc.engine.search;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mattlang.jc.Main.initLogging;
+
+import java.io.IOException;
 
 import org.junit.Test;
 import org.mattlang.jc.Factory;
-import org.mattlang.jc.board.*;
+import org.mattlang.jc.board.BoardRepresentation;
+import org.mattlang.jc.board.Color;
+import org.mattlang.jc.board.GameState;
+import org.mattlang.jc.board.Move;
 import org.mattlang.jc.engine.MoveList;
-import org.mattlang.jc.engine.evaluation.BoardStats;
-import org.mattlang.jc.engine.evaluation.DefaultEvaluateFunction;
-import org.mattlang.jc.engine.evaluation.SimpleBoardStatsGenerator;
-import org.mattlang.jc.engine.evaluation.Weights;
 import org.mattlang.jc.movegenerator.LegalMoveGenerator;
 import org.mattlang.jc.movegenerator.LegalMoveGeneratorImpl3;
 import org.mattlang.jc.uci.GameContext;
+import org.mattlang.jc.uci.UCI;
 
 public class SimpleNegaMaxEvalTest {
 
 
-
-
     @Test
-    public void testPatt() {
-        BoardRepresentation board = new Board3();
-        board.setFenPosition("position fen 7k/8/6Q1/8/3K4/8/8/8 b k - 2 17 ");
-
-        System.out.println(board.toUniCodeStr());
-
-        SimpleBoardStatsGenerator sbsg = new SimpleBoardStatsGenerator();
-        BoardStats stats = sbsg.gen(board, Color.BLACK);
-
-        DefaultEvaluateFunction eval = new DefaultEvaluateFunction();
-        int score = eval.eval(board, Color.WHITE);
-        System.out.println(score);
-        assertThat(score).isEqualTo(-Weights.PATT_WEIGHT);
-    }
-
-
-    @Test
-    public void analyzePattProblem() {
+    public void analyzePattProblem() throws IOException {
+        initLogging();
+        UCI.instance.attachStreams();
+        
         BoardRepresentation board = Factory.getDefaults().boards.create();
         GameState gameState = board.setFenPosition("position fen 8/8/8/4K3/8/6k1/8/3q4 w - - 14 1 moves e5f5 d1d5 f5f6 g3f2 f6e7 d5e5 e7d7 e5f6 d7c7 f6e6 c7b7 e6d6 b7a7 d6c6 a7b8 c6d7 b8a8");
 
