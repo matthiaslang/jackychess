28bbc6deb4283389db1ad0b2ec43ab836aa6af7e
lang <mla@mailbox.org>
2021-08-28 14:17:05 +0200

    fix: UCI wtime, btime, winc, binc has not been respected in absense of movestogo

--- src/main/java/org/mattlang/jc/uci/AsyncEngine.java
+++ src/main/java/org/mattlang/jc/uci/AsyncEngine.java
@@ -13,7 +13,7 @@ import org.mattlang.jc.board.Color;
 import org.mattlang.jc.board.GameState;
 import org.mattlang.jc.board.Move;
 import org.mattlang.jc.engine.Engine;
-
+import org.mattlang.jc.engine.evaluation.PhaseCalculator;
 
 public class AsyncEngine {
 
@@ -44,26 +44,31 @@ public class AsyncEngine {
 
         // if we have special "go" parameters, then override thinktime:
         if (!goParams.infinite) {
-            long restTime;
-            long restMoves;
-            if (gameState.getWho2Move() == Color.WHITE) {
-                restTime = goParams.wtime;
-            } else {
-                restTime = goParams.btime;
-            }
-            restMoves = goParams.movestogo;
-            if (restMoves != 0 && restTime > 0) {
-                long averageTimeForThisMove = restTime / restMoves;
-                // subtract a bit to not overceed the time:
-                if (averageTimeForThisMove>1000) {
-                    averageTimeForThisMove-=500;
-                }
-                searchParams.getConfig().timeout.setValue((int)averageTimeForThisMove);
-            }
             if (goParams.movetime > 0) {
-                searchParams.getConfig().timeout.setValue((int)goParams.movetime);
-            }
+                searchParams.getConfig().timeout.setValue((int) goParams.movetime);
+            } else {
 
+                long restTime;
+                long incTime;
+                long opponentRestTime;
+                long opponentIncTime;
+                long restMoves;
+                if (gameState.getWho2Move() == Color.WHITE) {
+                    restTime = goParams.wtime;
+                    incTime = goParams.winc;
+                    opponentRestTime = goParams.btime;
+                    opponentIncTime = goParams.binc;
+                } else {
+                    restTime = goParams.btime;
+                    incTime = goParams.binc;
+                    opponentRestTime = goParams.wtime;
+                    opponentIncTime = goParams.winc;
+                }
+                restMoves = goParams.movestogo;
+                long timeToUse =
+                        determineTime(gameState, restTime, opponentRestTime, incTime, opponentIncTime, restMoves);
+                searchParams.getConfig().timeout.setValue((int) timeToUse);
+            }
         }
         Factory.setDefaults(searchParams);
         Factory.getDefaults().log();
@@ -76,7 +81,7 @@ public class AsyncEngine {
                 return move;
             } catch (Exception e) {
                 completableFuture.completeExceptionally(e);
-                logger.log(SEVERE,"error during async execution!", e);
+                logger.log(SEVERE, "error during async execution!", e);
                 throw e;
             }
 
@@ -85,6 +90,72 @@ public class AsyncEngine {
         this.future = future;
         result = completableFuture;
         return result;
+    }
+
+    private long determineTime(GameState gameState, long restTime, long opponentRestTime, long incTime,
+            long opponentIncTime,
+            long restMoves) {
+        
+        long time = incTime / 2;
+
+        long diff = restTime - opponentRestTime;
+        if (diff > 0) {
+            time += diff / 3;
+        } else {
+            time -= diff / 3;
+        }
+
+        if (restMoves != 0 && restTime > 0) {
+            long averageTimeForThisMove = restTime / restMoves;
+
+            time += averageTimeForThisMove;
+        } else {
+            // determine by gamephase
+            double phase = PhaseCalculator.calcPhaseFactor(gameState.getBoard());
+            boolean isOpening = phase > 0.6;
+            if (isOpening) {
+                // estimate moves by phase:
+                // lets say a game has 60 moves in average:
+                int averageMovesPerGame = 60;
+                // weight by phase where we are
+                averageMovesPerGame = (int) (averageMovesPerGame * phase + 5);
+
+                long averageTime = restTime / averageMovesPerGame;
+
+                time += averageTime;
+            } else {
+                // average end game moves..
+                int averageMovesEndGame = 20;
+                long averageTime = restTime / averageMovesEndGame;
+                time += averageTime;
+            }
+        }
+
+        // subtract a bit to not overceed the time by engine stop delays:
+        if (time > 1000) {
+            time -= 200;
+        }
+
+        // fallback:
+        if (time <= 0) {
+            time = 1000;
+        }
+
+        // double check chosen time:
+        int approxamatelyFutureMoves = (int) (restTime / time);
+        if (approxamatelyFutureMoves < 5) {
+            time = restTime / 10;
+        } else if (approxamatelyFutureMoves < 10) {
+            time /= 2;
+        }
+
+        if (time > restTime) {
+            time = restTime;
+            if (time > 800) {
+                time -= 200;
+            }
+        }
+        return time;
     }
 
     public Optional<Move> stop() {
