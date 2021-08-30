50b1d4681ba5e48577db033337dda3982bae59fe
lang <mla@mailbox.org>
2021-08-30 10:33:53 +0200

    fixed timeout option weirdly multipliying by 1000 to calc between ms and s

--- src/main/java/org/mattlang/jc/uci/UCITimeoutOption.java
+++ src/main/java/org/mattlang/jc/uci/UCITimeoutOption.java
@@ -5,17 +5,7 @@ import java.util.Map;
 public class UCITimeoutOption extends UCISpinOption {
 
     public UCITimeoutOption(Map<String, UCIOption> optionBundle) {
-        super(optionBundle, "thinktime", 5, 600, 15);
-    }
-
-    @Override
-    public Integer getValue() {
-        return super.getValue() < 1000 ? super.getValue() * 1000 : super.getValue();
-    }
-
-    @Override
-    public void setValue(Integer newValue) {
-        super.setValue(newValue < 1000 ? newValue * 1000 : newValue);
+        super(optionBundle, "thinktime", 1000, 600000, 15000);
     }
 
 }
