71b4104caeb4db7ff94ea1d6ff77621d20d438c9
lang <mla@mailbox.org>
2021-09-28 14:13:26 +0200

    configurable log directory

--- src/main/java/org/mattlang/jc/AppConfiguration.java
+++ src/main/java/org/mattlang/jc/AppConfiguration.java
@@ -8,10 +8,12 @@ import java.util.Properties;
 
 /**
  * Bundles all engine configurations.
+ * Either set via system properties or loaded by a property file.
  */
 public class AppConfiguration {
 
     public static final String LOGGING_ACTIVATE = "jacky.logging.activate";
+    public static final String LOGGING_DIR = "jacky.logging.dir";
 
     public static final String CONFIG_FILE = "jacky.config";
 
--- src/main/java/org/mattlang/jc/Main.java
+++ src/main/java/org/mattlang/jc/Main.java
@@ -1,9 +1,10 @@
 package org.mattlang.jc;
 
-import static org.mattlang.jc.AppConfiguration.APPCONFIG;
-import static org.mattlang.jc.AppConfiguration.LOGGING_ACTIVATE;
+import static java.util.stream.Collectors.joining;
+import static org.mattlang.jc.AppConfiguration.*;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.Optional;
 import java.util.logging.LogManager;
 
 import org.mattlang.jc.uci.UCI;
@@ -30,9 +31,27 @@ public class Main {
         LogManager manager = LogManager.getLogManager();
         try {
             manager.reset();
-            manager.readConfiguration(Main.class.getResourceAsStream(resourceFile));
+            String configFileContent = loadFile(Main.class.getResourceAsStream(resourceFile));
+            configFileContent = replaceVariables(configFileContent);
+            ByteArrayInputStream bis = new ByteArrayInputStream(configFileContent.getBytes());
+            manager.readConfiguration(bis);
         } catch (IOException e) {
             throw new RuntimeException("Error reading logger config file: " + resourceFile);
         }
+    }
+
+    private static String replaceVariables(String configFileContent) {
+        Optional<String> optLogDir = APPCONFIG.getStringValue(LOGGING_DIR);
+        // either use a cofigured directory or the java.util.logging placeholder %h for user home:
+        String logDir = optLogDir.isPresent() ? optLogDir.get() : "%h";
+        configFileContent = configFileContent.replace("%h", logDir);
+
+        return configFileContent;
+    }
+
+    private static String loadFile(InputStream inputStream) {
+        return new BufferedReader(new InputStreamReader(inputStream))
+                .lines()
+                .collect(joining("\n"));
     }
 }
