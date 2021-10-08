6ea8aa238875035498b9a5e265b83c53631418aa
lang <mla@mailbox.org>
2021-10-08 14:03:41 +0200

    fix shifting issues with signed long

--- src/main/java/org/mattlang/jc/board/bitboard/BB.java
+++ src/main/java/org/mattlang/jc/board/bitboard/BB.java
@@ -14,7 +14,7 @@ public class BB {
     public static final long notHFile = 0x7f7f7f7f7f7f7f7fL;
 
     public static final long soutOne(long b) {
-        return b >> 8;
+        return b >>> 8;
     }
 
     public static final long nortOne(long b) {
@@ -32,15 +32,15 @@ public class BB {
     }
 
     public static final long soEaOne(long b) {
-        return (b >> 7) & notAFile;
+        return (b >>> 7) & notAFile;
     }
 
     public static final long westOne(long b) {
-        return (b >> 1) & notHFile;
+        return (b >>> 1) & notHFile;
     }
 
     public static final long soWeOne(long b) {
-        return (b >> 9) & notHFile;
+        return (b >>> 9) & notHFile;
     }
 
     public static final long noWeOne(long b) {
