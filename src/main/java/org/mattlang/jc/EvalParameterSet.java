f66ad885575fd65c5b9578715af6a8ec9d3f4e2c
lang <mla@mailbox.org>
2021-12-02 11:21:13 +0100

    first simple parameterized evaluation config

--- src/main/java/org/mattlang/jc/EvalParameterSet.java
+++ src/main/java/org/mattlang/jc/EvalParameterSet.java
@@ -1,7 +1,17 @@
 package org.mattlang.jc;
 
+/**
+ * Different evaluation parameter sets to play and test around.
+ */
 public enum EvalParameterSet {
 
+    /**
+     * only Material Evaluation.
+     */
     DEFAULT,
 
+    /**
+     * Material and Mobility Evaluation.
+     */
+    MATANDMOB
 }
