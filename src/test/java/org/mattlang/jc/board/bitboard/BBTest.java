b82fb0f6b3e271d110290a68317eeb8ebc5c002c
lang <mla@mailbox.org>
2021-12-20 10:25:43 +0100

    fixed bit overflow issue with fillFile

--- src/test/java/org/mattlang/jc/board/bitboard/BBTest.java
+++ src/test/java/org/mattlang/jc/board/bitboard/BBTest.java
@@ -5,6 +5,35 @@ import org.junit.Test;
 public class BBTest {
 
     @Test
+    public void testFileFill(){
+        long rookMask = 1L << 63;
+
+        long fileFilled = BB.fileFill(rookMask);
+
+        System.out.println("Pattern \n" + BitChessBoard.toStr(fileFilled));
+    }
+
+    @Test
+    public void testPatterns(){
+        long A = 0x0101010101010101L;
+        long B = 0x0202020202020202L;
+        long C = 0x0404040404040404L;
+        long D = 0x0808080808080808L;
+        long E = 0x1010101010101010L;
+        long F = 0x2020202020202020L;
+        long G = 0x4040404040404040L;
+        long H = 0x8080808080808080L;
+
+        
+        System.out.println("Pattern \n" + BitChessBoard.toStr(A*2*2));
+    }
+
+    @Test
+    public void testPatterns2(){
+        long l = 0b0000000000000000L;
+        System.out.println("Pattern \n" + BitChessBoard.toStr(BB.FGH_File));
+    }
+    @Test
     public void testKingAttacks() {
 
         for (int i = 0; i < 64; i++) {
