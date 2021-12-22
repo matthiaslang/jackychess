56e8d77162e851d6ee990946a990fd21c2580706
lang <mla@mailbox.org>
2021-12-22 20:44:36 +0100

    version contains git describe

--- src/main/java/org/mattlang/jc/uci/UciProcessor.java
+++ src/main/java/org/mattlang/jc/uci/UciProcessor.java
@@ -163,7 +163,7 @@ public class UciProcessor {
     }
 
     private void identifyYourself() {
-        String version = Factory.getAppProps().getProperty("version");
+        String version = Factory.getAppProps().getProperty("longversion");
         String buildtime = Factory.getAppProps().getProperty("buildtime");
         UCI.instance.putCommand("id name JackyChess " + version + " build at: " + buildtime);
         UCI.instance.putCommand("id author Matthias Lang");
