3f5f92d59b0503abe79bc58dcb73ef9ab0898297
lang <mla@mailbox.org>
2022-02-25 10:41:48 +0100

    experimental int based zobrist key cache; not yet used

---
+++ src/main/java/org/mattlang/jc/engine/tt/IntIntCache.java
@@ -0,0 +1,52 @@
+package org.mattlang.jc.engine.tt;
+
+/**
+ * A thread safe tt cache with an int as payload and an int storing the zobrist key (partial).
+ */
+public final class IntIntCache {
+
+    private int bitSize;
+    private int capacity;
+
+    public static final int NORESULT = 1000000;
+
+    private int[] zobrists;
+    private int[] scores;
+
+    public IntIntCache(int bitSize) {
+        this.bitSize = bitSize;
+        capacity = 1 << bitSize;
+        zobrists = new int[capacity];
+        scores = new int[capacity];
+    }
+
+    public int find(long zobristHash) {
+        int index = h0(zobristHash);
+        int partialZ = h1(zobristHash);
+        long xorKey = zobrists[index];
+        int score = scores[index];
+
+        if ((xorKey ^ score) == partialZ) {
+            return score;
+        }
+
+        return NORESULT;
+    }
+
+    private int h0(long key) {
+        return (int) (key & (capacity - 1));
+    }
+
+    private final int h1(long key) {
+        return (int) ((key >> 32) & (capacity - 1));
+    }
+
+    public void save(long zobristHash, int score) {
+        int index = h0(zobristHash);
+        int partialZ = h1(zobristHash);
+
+        zobrists[index] = partialZ ^ score;
+        scores[index] = score;
+
+    }
+}
