380e4cb9f448eed35022501f75e9afa2ecc08b14
lang <mla@mailbox.org>
2022-03-01 10:45:38 +0100

    small board print method

--- src/main/java/org/mattlang/jc/board/BoardPrinter.java
+++ src/main/java/org/mattlang/jc/board/BoardPrinter.java
@@ -44,13 +44,62 @@ public class BoardPrinter {
         return b.toString();
     }
 
-
     private static void addSeparator(StringBuilder b) {
         b.append(" ");
-        for(int i=0; i<8; i++) {
+        for (int i = 0; i < 8; i++) {
             b.append("+---");
         }
         b.append("+\n");
     }
 
+    public static void printSmallBoard(BoardRepresentation board) {
+        System.out.println(toSmallBoardStr(board));
+    }
+
+    private static String toSmallBoardStr(BoardRepresentation board) {
+        String s1 = toSmallStr((row, col) -> board.getPos(row, col));
+
+        // combine the two board prints:
+        StringBuilder b = new StringBuilder();
+        String[] r1 = s1.split("\n");
+        for (int i = 0; i < r1.length; i++) {
+            b.append(r1[i] + "          ");
+            b.append("\n");
+        }
+        return b.toString();
+    }
+
+    public static String toSmallStr(BiFunction<Integer, Integer, Character> posFunction) {
+        StringBuilder b = new StringBuilder();
+        int rowNo = 8;
+        addSmallSeparator(b);
+        for (int row = 0; row < 8; row++) {
+
+            b.append(rowNo);
+
+            rowNo--;
+            b.append("|");
+            for (int col = 0; col < 8; col++) {
+                b.append(" ");
+                Character figure = posFunction.apply(row, col);
+                if (figure.charValue() == ' ') {
+                    figure = '.';
+                }
+                b.append(figure);
+
+            }
+            b.append("|\n");
+        }
+        addSmallSeparator(b);
+        b.append("   a b c d e f g h  \n");
+        return b.toString();
+    }
+
+    private static void addSmallSeparator(StringBuilder b) {
+        b.append(" ");
+        for (int i = 0; i < 8; i++) {
+            b.append("-+");
+        }
+        b.append("+\n");
+    }
 }
