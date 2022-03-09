dd5cd45f9f4136db1caaacc6249554562242b8f9
lang <mla@mailbox.org>
2022-03-09 08:25:43 +0100

    fixed example code test

--- src/test/java/org/mattlang/jc/moves/MoveBoardIteratorTest.java
+++ src/test/java/org/mattlang/jc/moves/MoveBoardIteratorTest.java
@@ -2,16 +2,19 @@ package org.mattlang.jc.moves;
 
 import org.junit.Test;
 import org.mattlang.jc.board.BoardRepresentation;
+import org.mattlang.jc.board.bitboard.BitBoard;
 import org.mattlang.jc.engine.CheckChecker;
 import org.mattlang.jc.engine.MoveList;
+import org.mattlang.jc.movegenerator.BBCheckCheckerImpl;
 
 public class MoveBoardIteratorTest {
 
     @Test
     public void testiterating() {
         MoveList moves = new MoveListImpl();
-        BoardRepresentation board = null;
-        CheckChecker checkchecker = null;
+        BoardRepresentation board = new BitBoard();
+        board.setStartPosition();
+        CheckChecker checkchecker = new BBCheckCheckerImpl();
         try (MoveBoardIterator mbi = new MoveBoardIterator(moves.iterate(), board, checkchecker)) {
             while (mbi.doNextMove()) {
 
