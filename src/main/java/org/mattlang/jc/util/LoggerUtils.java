1c01f99ebb7542bbe670c474fd13615e3e891dd4
lang <mla@mailbox.org>
2022-05-31 10:04:12 +0200

    logging stats

--- src/main/java/org/mattlang/jc/util/LoggerUtils.java
+++ src/main/java/org/mattlang/jc/util/LoggerUtils.java
@@ -1,5 +1,11 @@
 package org.mattlang.jc.util;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.mattlang.jc.Factory;
 import org.mattlang.jc.board.GameState;
 
@@ -16,5 +22,36 @@ public class LoggerUtils {
         Factory.getDefaults().log(b);
 
         return b.toString();
+    }
+
+    public static void logStats(Logger logger, String msg, Map<String, Object> stats) {
+        if (logger.isLoggable(Level.FINE)) {
+            StringWriter sw = new StringWriter();
+            try (PrintWriter writer = new PrintWriter(sw)) {
+                logStatsRecursive(writer, 0, stats);
+            }
+            logger.fine(msg + "\n" + sw.toString());
+        }
+    }
+
+    private static void logStatsRecursive(PrintWriter writer, int prefix, Map<String, Object> stats) {
+
+        for (Map.Entry<String, Object> entry : stats.entrySet()) {
+            if (entry.getValue() instanceof Map) {
+                tabs(writer, prefix);
+                writer.println(entry.getKey());
+                logStatsRecursive(writer, prefix + 2, (Map<String, Object>) entry.getValue());
+            } else {
+                tabs(writer, prefix);
+                writer.println(entry.getKey() + " = " + entry.getValue());
+            }
+        }
+
+    }
+
+    private static void tabs(PrintWriter writer, int prefix) {
+        for (int i = 0; i < prefix; i++) {
+            writer.write(" ");
+        }
     }
 }
