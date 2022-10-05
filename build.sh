002385762d05177b594a058d8ec7be9eead5ab58
lang <mla@mailbox.org>
2022-10-05 14:26:51 +0200

    build helper file: create *.bat file for local testing with arena

--- build.sh
+++ build.sh
@@ -5,13 +5,25 @@
 # build
 mvn clean package -DskipTests
 
+MVNVERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+
+JARFILE=jackychess-${MVNVERSION}.jar
+
 # copy to our folders where the test programs have access:
 echo "copy to test folders"
 cp -v target/jackychess*   ../jcversions/
 cp -v target/jackychess*   ../jackyChessDockerTesting/jackychess
 
+
+# copy a windows bat file for the arena test folder with some log settings
+BATFILE=jackychess-${MVNVERSION}.bat
+cat << EOF > ../jcversions/${BATFILE}
+java  -Xmx1024M  -Djacky.logging.activate=true -Djacky.logging.dir=c:/logs  -Djacky.logging.level=INFO -Djacky.logging.file=jc-${MVNVERSION}  -jar ${JARFILE}
+EOF
+
+
 # create a temporary tag:
-MVNVERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+
 
 #GITDESCR=$(git describe --tags)
 GITDESCR=$(git rev-parse --short HEAD)
