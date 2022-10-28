4b24f7be2ccb04b4bac060f749a06a669a59b65d
lang <mla@mailbox.org>
2022-10-28 13:38:31 +0200

    write bat file

--- build.sh
+++ build.sh
@@ -5,14 +5,27 @@
 # build
 mvn clean package -DskipTests
 
+MVNVERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+
+JARFILE=jackychess-${MVNVERSION}.jar
+
 # copy to our folders where the test programs have access:
 echo "copy to test folders"
 cp -v target/jackychess*   ../jcversions/
 cp -v target/jackychess*   ../jackyChessDockerTesting/jackychess
 
-# create a temporary tag:
-MVNVERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+# copy a windows bat file for the arena test folder with some log settings
+BATFILE=jc-${MVNVERSION}.bat
+cat << EOF > ../jcversions/${BATFILE}
+java  -Xmx2024M  -Djacky.logging.activate=true -Djacky.logging.dir=c:/logs  -Djacky.logging.level=INFO -Djacky.logging.file=jc-${MVNVERSION}  -jar ${JARFILE}
 
+EOF
+# convert windows lfs:
+unix2dos  ../jcversions/${BATFILE}
+# make it executable; sets in cygwin the window right for execution on the file:
+chmod +x  ../jcversions/${BATFILE}
+
+# create a temporary tag:
 #GITDESCR=$(git describe --tags)
 GITDESCR=$(git rev-parse --short HEAD)
 
