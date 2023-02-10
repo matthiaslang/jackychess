e4b252d3973d104879f4988c2fe5a652f4329cd3
lang <mla@mailbox.org>
2023-02-10 08:24:37 +0100

    Test to measure evaluation performance

--- src/test/java/org/mattlang/jc/engine/evaluation/parameval/ParameterizedEvaluationTest.java
+++ src/test/java/org/mattlang/jc/engine/evaluation/parameval/ParameterizedEvaluationTest.java
@@ -2,12 +2,19 @@ package org.mattlang.jc.engine.evaluation.parameval;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.junit.Test;
 import org.mattlang.jc.EvalParameterSet;
 import org.mattlang.jc.Factory;
+import org.mattlang.jc.StopWatch;
+import org.mattlang.jc.TestPosition;
 import org.mattlang.jc.board.BoardRepresentation;
 import org.mattlang.jc.board.Color;
 import org.mattlang.jc.board.bitboard.BitBoard;
+import org.mattlang.jc.chessTests.EigenmannRapidEngineChessIT;
+import org.mattlang.jc.chessTests.EpdParsing;
 import org.mattlang.jc.engine.evaluation.parameval.functions.ArrayFunction;
 import org.mattlang.jc.engine.evaluation.parameval.functions.Function;
 
@@ -67,5 +74,34 @@ public class ParameterizedEvaluationTest {
 
         board.setFenPosition("position fen rnbqkbnr/pppppppp/8/8/8/3N4/PPPPPPPP/RNBQKBNR w - - 0 1");
         assertThat(pe.eval(board, Color.WHITE)).isEqualTo(322);
+    }
+
+//    @Test
+    public void testPerformance() {
+        Factory.getDefaults().getConfig().evaluateParamSet.setValue(EvalParameterSet.CURRENT);
+
+        // get an evaluation for tuning: no caching activated:
+        ParameterizedEvaluation pe = ParameterizedEvaluation.createForTuning();
+
+        List<TestPosition> positions = EpdParsing.convertTests(EigenmannRapidEngineChessIT.eret);
+
+        List<BoardRepresentation> boards = positions.stream()
+                .map(p -> {
+                    BoardRepresentation board = new BitBoard();
+                    board.setFenPosition(p.getFenPosition());
+                    return board;
+                }).collect(Collectors.toList());
+
+        StopWatch watch = new StopWatch();
+        watch.start();
+        int evals = 0;
+        for (int i = 0; i < 250000; i++) {
+            for (BoardRepresentation board : boards) {
+                evals += pe.eval(board, Color.WHITE);
+            }
+        }
+        watch.stop();
+        System.out.println(evals);
+        System.out.println(watch.toString());
     }
 }
