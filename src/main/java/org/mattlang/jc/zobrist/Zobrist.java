1509547fb33ccee2b77abdb7e510bfede099392e
lang <mla@mailbox.org>
2023-07-17 15:07:32 +0200

    ease zobrist logic

--- src/main/java/org/mattlang/jc/zobrist/Zobrist.java
+++ src/main/java/org/mattlang/jc/zobrist/Zobrist.java
@@ -12,11 +12,13 @@ public class Zobrist {
 
     // max field pos: 64 board files + 1 "virtual" für en passten unset (-1)
     private static final int MAXPOS = 64 + 1;
-    // 12 figures (6 white, 6 black) + 2 en passant
-    private static final int MAXFIG = 6 + 6 + 2;
+    // 12 figures encoded by their figure code + 2 en passant
+    private static final int MAXFIG = FT_ALL + BLACK.code + 2;
     // castling rights are saved in the board by a nibble, so use 16 rnd values
     private static final int MAXCASTLING = 16;
-    private static final int ENPASSANT_MOVE_TARGET_POS_IDX = 13;
+
+    /* last index is used for en passant encoding. */
+    private static final int ENPASSANT_MOVE_TARGET_POS_IDX = MAXFIG-1;
 
     private static long[][] rnd = new long[MAXPOS][MAXFIG];
 
@@ -67,23 +69,16 @@ public class Zobrist {
                 h = addFig(h, i, fig);
             }
         }
-//        if (board.getSiteToMove() == BLACK) {
-//            h = Zobrist.colorFlip(h);
-//        }
         return h;
     }
 
     public static long addFig(long h, int i, byte fig) {
-        int blackoffset = (fig & BLACK.code) == BLACK.code ? 6 : 0;
-        int figIndex = (fig & MASK_OUT_COLOR) + blackoffset;
-        h ^= rnd[i][figIndex];
+        h ^= rnd[i][fig];
         return h;
     }
 
     public static long removeFig(long h, int i, byte fig) {
-        int blackoffset = (fig & BLACK.code) == BLACK.code ? 6 : 0;
-        int figIndex = (fig & MASK_OUT_COLOR) + blackoffset;
-        h ^= rnd[i][figIndex];
+        h ^= rnd[i][fig];
         return h;
     }
 
