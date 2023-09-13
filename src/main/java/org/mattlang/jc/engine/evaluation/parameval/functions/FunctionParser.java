e1bf18addf5a86510f8ff35a00114f9e5588fa6e
lang <mla@mailbox.org>
2023-09-13 15:14:54 +0200

    clean up functions deleting obsolete code

--- src/main/java/org/mattlang/jc/engine/evaluation/parameval/functions/FunctionParser.java
+++ src/main/java/org/mattlang/jc/engine/evaluation/parameval/functions/FunctionParser.java
@@ -4,30 +4,6 @@ import org.mattlang.jc.engine.evaluation.parameval.ConfigParseException;
 
 public class FunctionParser {
 
-    public static Function parseFunction(String funStr) {
-        try {
-            return MobLinFun.parse(funStr);
-        } catch (ConfigParseException cpe) {
-            // ignore
-        }
-        try {
-            return TropismFun.parse(funStr);
-        } catch (ConfigParseException cpe) {
-            // ignore
-        }
-        try {
-            return KingAttackFun.parse(funStr);
-        } catch (ConfigParseException cpe) {
-            // ignore
-        }
-
-        try {
-            return ArrayFunction.parse(funStr);
-        } catch (ConfigParseException cpe) {
-            throw new ConfigParseException("Function not parseable!", cpe);
-        }
-    }
-
     public static ArrayFunction parseArray(String funStr) {
         try {
             return ArrayFunction.parse(funStr);
