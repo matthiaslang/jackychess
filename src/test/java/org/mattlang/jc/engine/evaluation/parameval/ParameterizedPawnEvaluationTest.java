14a5052d0aa9076eb1af6dc352ee6b5e3777e46b
lang <mla@mailbox.org>
2023-09-21 10:48:38 +0200

    add unit test

--- src/test/java/org/mattlang/jc/engine/evaluation/parameval/ParameterizedPawnEvaluationTest.java
+++ src/test/java/org/mattlang/jc/engine/evaluation/parameval/ParameterizedPawnEvaluationTest.java
@@ -1,32 +1,46 @@
 package org.mattlang.jc.engine.evaluation.parameval;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mattlang.jc.board.bitboard.BB.*;
+import static org.mattlang.jc.board.bitboard.BitChessBoard.nBlack;
+import static org.mattlang.jc.board.bitboard.BitChessBoard.nWhite;
 import static org.mattlang.jc.engine.evaluation.parameval.ParameterizedPawnEvaluation.getPawnNeighbours;
 
-import org.assertj.core.api.Assertions;
+import org.assertj.core.presentation.Representation;
+import org.assertj.core.presentation.StandardRepresentation;
 import org.junit.Test;
 import org.mattlang.jc.board.bitboard.BB;
 import org.mattlang.jc.board.bitboard.BitBoard;
-import org.mattlang.jc.board.bitboard.BitChessBoard;
 
 public class ParameterizedPawnEvaluationTest {
+
+    private static final Representation BITREP = new StandardRepresentation() {
+
+        @Override
+        public String toStringOf(Object object) {
+            if (object instanceof Long) {
+                return BB.toStrBoard((Long) object);
+            }
+            return super.toStringOf(object);
+        }
+
+        @Override
+        public String unambiguousToStringOf(Object object) {
+            return super.unambiguousToStringOf(object);
+        }
+    };
 
     @Test
     public void testPawnNeighbors() {
 
-        System.out.println(BB.toStr(BB.notAFile));
-        System.out.println(BB.toStr(BB.notHFile));
-
         BitBoard board = new BitBoard();
         board.setStartPosition();
 
-        long neighbours = getPawnNeighbours(board.getBoard().getPawns(BitChessBoard.nWhite));
-        System.out.println(BB.toStr(neighbours));
-        Assertions.assertThat(neighbours).isEqualTo(B2 | C2 | D2 | E2 | F2 | G2);
+        long neighbours = getPawnNeighbours(board.getBoard().getPawns(nWhite));
+        assertThat(neighbours).withRepresentation(BITREP).isEqualTo(B2 | C2 | D2 | E2 | F2 | G2);
 
-        neighbours = getPawnNeighbours(board.getBoard().getPawns(BitChessBoard.nBlack));
-        System.out.println(BB.toStr(neighbours));
-        Assertions.assertThat(neighbours).isEqualTo(B7 | C7 | D7 | E7 | F7 | G7);
+        neighbours = getPawnNeighbours(board.getBoard().getPawns(nBlack));
+        assertThat(neighbours).withRepresentation(BITREP).isEqualTo(B7 | C7 | D7 | E7 | F7 | G7);
     }
 
     @Test
@@ -36,16 +50,39 @@ public class ParameterizedPawnEvaluationTest {
         board.setFenPosition("position fen K7/8/4p3/8/8/4PP2/8/7k w - - 1 56 ");
         board.println();
 
-        long neighbours = getPawnNeighbours(board.getBoard().getPawns(BitChessBoard.nWhite));
-        System.out.println(BB.toStrBoard(neighbours));
-        System.out.println(BB.toStrBoard(D3 | E3 | F3 | G3));
-        Assertions.assertThat(neighbours).isEqualTo(D3 | E3 | F3 | G3);
+        long neighbours = getPawnNeighbours(board.getBoard().getPawns(nWhite));
+        assertThat(neighbours).withRepresentation(BITREP).isEqualTo(D3 | E3 | F3 | G3);
 
-        neighbours = getPawnNeighbours(board.getBoard().getPawns(BitChessBoard.nBlack));
-        System.out.println(BB.toStrBoard(neighbours));
-        Assertions.assertThat(neighbours).isEqualTo(D6 | F6);
+        neighbours = getPawnNeighbours(board.getBoard().getPawns(nBlack));
+        assertThat(neighbours).withRepresentation(BITREP).isEqualTo(D6 | F6);
     }
 
+    @Test
+    public void testPawnNeighbors2() {
 
+        BitBoard board = new BitBoard();
+        board.setFenPosition("position fen K7/8/p1p1p1p1/8/8/1P1P1P1P/8/7k w - - 1 56 ");
+        board.println();
+
+        long neighbours = getPawnNeighbours(board.getBoard().getPawns(nWhite));
+        assertThat(neighbours).withRepresentation(BITREP).isEqualTo(C3 | E3 | G3);
+
+        neighbours = getPawnNeighbours(board.getBoard().getPawns(nBlack));
+        assertThat(neighbours).withRepresentation(BITREP).isEqualTo(B6 | D6 | F6);
+    }
+
+    @Test
+    public void testPawnNeighbors3() {
+
+        BitBoard board = new BitBoard();
+        board.setFenPosition("position fen K7/8/4p3/8/8/3P4/8/7k w - - 1 56 ");
+        board.println();
+
+        long neighbours = getPawnNeighbours(board.getBoard().getPawns(nWhite));
+        assertThat(neighbours).withRepresentation(BITREP).isEqualTo(C3 | E3);
+
+        neighbours = getPawnNeighbours(board.getBoard().getPawns(nBlack));
+        assertThat(neighbours).withRepresentation(BITREP).isEqualTo(D6 | F6);
+    }
 
 }
