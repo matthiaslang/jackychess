80ca4fce0cd507969ee3187f6d3cc291b1bdc37c
lang <mla@mailbox.org>
2024-04-30 14:50:29 +0200

    benchmark to try out eval opts

--- engine/src/test/java/org/mattlang/benchmarks/BenchmarkTryout.java
+++ engine/src/test/java/org/mattlang/benchmarks/BenchmarkTryout.java
@@ -1,6 +1,14 @@
 package org.mattlang.benchmarks;
 
-import org.mattlang.jc.engine.evaluation.Tools;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mattlang.jc.board.BoardRepresentation;
+import org.mattlang.jc.board.Color;
+import org.mattlang.jc.board.bitboard.BitBoard;
+import org.mattlang.jc.chessTests.BratKoKopecIT;
+import org.mattlang.jc.chessTests.EpdParsing;
+import org.mattlang.jc.engine.evaluation.parameval.ParameterizedEvaluation;
 import org.openjdk.jmh.annotations.*;
 
 public class BenchmarkTryout {
@@ -10,29 +18,59 @@ public class BenchmarkTryout {
 
         public int x = 17;
         public int y = 34;
+
+        public ParameterizedEvaluation pe = new ParameterizedEvaluation();
+        public BoardRepresentation board = new BitBoard();
+
+        public List<BoardRepresentation> boards = new ArrayList<BoardRepresentation>();
+        {
+            pe.setCaching(false);
+
+            // add start position:
+            board.setFenPosition("position fen rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w - - 0 1");
+            boards.add(board);
+            // add the brat co test positions:
+            for (int i = 0; i < 10000; i++) {
+                for (String[] epdTest : EpdParsing.getEPDTests(BratKoKopecIT.bratkoKopec)) {
+                    BoardRepresentation board = new BitBoard();
+                    board.setFenPosition("position fen " + epdTest[0] + " 0 0");
+                    boards.add(board);
+                }
+            }
+        }
     }
 
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
-    public int ToolsDistanceDirekt(Constants constants) {
-        int sum = 0;
-        for (int i = 0; i < constants.x; i++) {
-            for (int j = 0; j < constants.y; j++) {
-                sum += Tools.calcDistance(i, j);
-            }
+    public int measureEvaluation(Constants constants) {
+        int val=0;
+        for (BoardRepresentation board : constants.boards) {
+            val+= constants.pe.eval(board, Color.WHITE);
         }
-        return sum;
+        return val;
     }
 
-    @Benchmark
-    @BenchmarkMode(Mode.AverageTime)
-    public int ToolsDistanceCached(Constants constants) {
-        int sum = 0;
-        for (int i = 0; i < constants.x; i++) {
-            for (int j = 0; j < constants.y; j++) {
-                sum += Tools.distance(i, j);
-            }
-        }
-        return sum;
-    }
+    //    @Benchmark
+    //    @BenchmarkMode(Mode.AverageTime)
+    //    public int ToolsDistanceDirekt(Constants constants) {
+    //        int sum = 0;
+    //        for (int i = 0; i < constants.x; i++) {
+    //            for (int j = 0; j < constants.y; j++) {
+    //                sum += Tools.calcDistance(i, j);
+    //            }
+    //        }
+    //        return sum;
+    //    }
+    //
+    //    @Benchmark
+    //    @BenchmarkMode(Mode.AverageTime)
+    //    public int ToolsDistanceCached(Constants constants) {
+    //        int sum = 0;
+    //        for (int i = 0; i < constants.x; i++) {
+    //            for (int j = 0; j < constants.y; j++) {
+    //                sum += Tools.distance(i, j);
+    //            }
+    //        }
+    //        return sum;
+    //    }
 }
