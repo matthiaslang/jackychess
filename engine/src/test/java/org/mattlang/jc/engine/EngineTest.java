1631a126d39adbfec7be44545750ddcbb25fec82
lang <mla@mailbox.org>
2024-06-12 12:40:28 +0200

    fixed test case due to retuned parameter

--- engine/src/test/java/org/mattlang/jc/engine/EngineTest.java
+++ engine/src/test/java/org/mattlang/jc/engine/EngineTest.java
@@ -179,7 +179,7 @@ public class EngineTest {
         assertThat(move).isEqualTo(bestm[0]);
 
         // check result; of course this could change if evaluation changes
-        assertThat(move.toStr()).isEqualTo("b8c6");
+        assertThat(move.toStr()).isEqualTo("e7e5");
     }
 
     /**
