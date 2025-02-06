1c5eafd90a2919da8b58537ca47595c25e65b741
lang <mla@mailbox.org>
2025-02-06 11:42:42 +0100

    changed tt cache index calculation to have better distribution/usage of cache

--- engine/src/main/java/org/mattlang/jc/engine/tt/TTCache.java
+++ engine/src/main/java/org/mattlang/jc/engine/tt/TTCache.java
@@ -10,6 +10,8 @@ import org.mattlang.jc.ConfigValues;
 import org.mattlang.jc.board.BoardRepresentation;
 import org.mattlang.jc.board.Color;
 
+import lombok.Getter;
+
 /**
  * Cache using only a long array to be faster and more memory efficient.
  * It is also thread-safe (thread consistent for read access), so that it could be used for a Lazy-SMP Search algorithm.
@@ -43,10 +45,27 @@ public final class TTCache {
     private static final int MOVE = 12; // 22
     private static final int SCORE = 48; // 16
 
+    @Getter
     private long cacheHits = 0;
+    @Getter
     private long cacheMisses = 0;
 
+    @Getter
+    private int numFilledEntries = 0;
+    @Getter
+    private int noReplaceCacheAlreadyBetter;
+
     private int mbSize = DEFAULT_CACHE_SIZE_MB;
+
+    /**
+     * no. of "places" in the index. effectively the array size div bucketChunkSize.
+     */
+    private int indexPlaces;
+
+    /**
+     * Size of a "chunk" consisting of the data of all buckets of an index place.
+     */
+    private int bucketChunkSize;
 
     private int determineBitSizeFromConfig() {
         int mb = getConfiguredMbSize();
@@ -83,11 +102,16 @@ public final class TTCache {
         LOGGER.info("TT Cache: allocating " + maxEntries + " longs;");
 
         keys = new long[maxEntries];
+        bucketChunkSize = BUCKET_SIZE * 2;
+        // entries is size div bucket size div data size.
+        indexPlaces = keys.length / bucketChunkSize;
     }
 
     public void reset() {
         Arrays.fill(keys, 0);
         halfMoveCounter = 0;
+        numFilledEntries = 0;
+        resetStatistics();
         aging.reset();
         checkUpdateCacheSize();
     }
@@ -109,8 +133,16 @@ public final class TTCache {
         return NORESULT;
     }
 
+    /**
+     * Calculates the index place in the data.
+     * @param key
+     * @return
+     */
     private int getIndex(final long key) {
-        return (int) (key >>> keyShifts) << 1;
+        // xor upper and lower halves of zobrist together and apply mask to have positive values:
+        long index = (key ^ (key >>> 32)) & 0x7FFFFFFF;
+        // use modulo to ensure better distribution of the values over the array.
+        return (int) (index % indexPlaces) * bucketChunkSize;
     }
 
     public void addValue(final long key, int score, final int depth, final int flag, final int move) {
@@ -123,6 +155,7 @@ public final class TTCache {
             long xorKey = keys[i];
             if (xorKey == 0) {
                 replaceIndex = i;
+                numFilledEntries++;
                 break;
             }
 
@@ -131,6 +164,7 @@ public final class TTCache {
             int currentDepth = getDepth(currentValue);
             if ((xorKey ^ currentValue) == key) {
                 if (currentDepth > depth && flag != TTResult.EXACT_VALUE) {
+                    noReplaceCacheAlreadyBetter++;
                     return;
                 }
                 replaceIndex = i;
@@ -178,14 +212,67 @@ public final class TTCache {
                 + getFlag(ttValue);
     }
 
+    /**
+     * Gives a raw statistical usage by inspecting the first 1000 entries.
+     * If the cache is well distributing the values this gives a good match of the overal usage.
+     *
+     * @return
+     */
     public long getUsagePercentage() {
         int usage = 0;
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 2000; i += 2) {
             if (keys[i] != 0) {
                 usage++;
             }
         }
         return usage;
+    }
+
+    /**
+     * Calculates the real usage of the cache inspecting all entries. Since this takes more time, this is only
+     * used in test cases to inspect real cache usage.
+     *
+     * @return
+     */
+    public long getPreciseUsagePercentage() {
+        int usage = 0;
+        for (int i = 0; i < keys.length; i += 2) {
+            if (keys[i] != 0) {
+                usage++;
+            }
+        }
+        return (usage * 1000 / (keys.length / 2));
+
+    }
+
+    /**
+     * Calculates a heuristic about the overal usage distribution of the cache.
+     * Used in testcases to verify proper distribution of cache values.
+     *
+     * @return
+     */
+    public int[] fillHeuristic() {
+
+        int chunks = 100;
+        int[] heuristics = new int[chunks];
+        int chunkSize = keys.length / chunks;
+
+        for (int chunk = 0; chunk < chunks; chunk++) {
+            int from = chunk * chunkSize;
+            int to = from + chunkSize;
+            heuristics[chunk] = calcChunkHeuristic(from, to, chunkSize);
+        }
+        return heuristics;
+    }
+
+    private int calcChunkHeuristic(int from, int to, int chunkSize) {
+        int usage = 0;
+        for (int i = from; i < to; i += 2) {
+            if (keys[i] != 0) {
+                usage++;
+            }
+        }
+        return (usage * 1000 / (chunkSize / 2));
     }
 
     public boolean findEntry(TTResult result, BoardRepresentation board) {
@@ -230,5 +317,11 @@ public final class TTCache {
 
     public int getCacheSize() {
         return keys.length * 8;
+    }
+
+    public void resetStatistics() {
+        cacheHits = 0;
+        cacheMisses = 0;
+        noReplaceCacheAlreadyBetter = 0;
     }
 }
---
+++ engine/src/test/java/org/mattlang/jc/perftests/CachePerformanceTests.java
@@ -0,0 +1,125 @@
+package org.mattlang.jc.perftests;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mattlang.jc.SearchParameter.params;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mattlang.SlowTests;
+import org.mattlang.jc.SearchParameter;
+import org.mattlang.jc.TestTools;
+import org.mattlang.jc.board.GameState;
+import org.mattlang.jc.board.Move;
+import org.mattlang.jc.engine.Engine;
+import org.mattlang.jc.engine.evaluation.parameval.functions.ArrayFunction;
+import org.mattlang.jc.engine.tt.Caching;
+import org.mattlang.jc.engine.tt.TTCache;
+import org.mattlang.jc.uci.GameContext;
+import org.mattlang.jc.util.Logging;
+
+import lombok.Data;
+
+/**
+ * Testcode to analyze tt cache.
+ */
+@Category(SlowTests.class)
+public class CachePerformanceTests {
+
+    @Data
+    public static class CacheInfo {
+
+        private final int noReplaceCacheAlreadyBetter;
+        private final long cacheMisses;
+        private final long cacheHits;
+        private final int cacheSize;
+        private final long usage;
+        private final int numFieldEntries;
+        private final int numMaxEntries;
+        private final long preciseUsage;
+        private final int[] heuristic;
+
+        public CacheInfo(TTCache ttCache) {
+            cacheSize = ttCache.getCacheSize();
+            cacheHits = ttCache.getCacheHits();
+            cacheMisses = ttCache.getCacheMisses();
+            noReplaceCacheAlreadyBetter = ttCache.getNoReplaceCacheAlreadyBetter();
+            usage = ttCache.getUsagePercentage();
+            preciseUsage = ttCache.getPreciseUsagePercentage();
+            numFieldEntries = ttCache.getNumFilledEntries();
+            numMaxEntries = cacheSize / 8 / 2;
+            heuristic = ttCache.fillHeuristic();
+        }
+
+        public void printInfo() {
+            System.out.println("Cache: hits: " + cacheHits + " misses: " + cacheMisses + " noreplace: "
+                    + noReplaceCacheAlreadyBetter);
+            System.out.println(
+                    "Cache usage: " + usage + " precise usage: " + preciseUsage + " numFieldEntries: " + numFieldEntries
+                            + " max: " + numMaxEntries + " numField ratio permille: " + (numFieldEntries * 1000
+                            / numMaxEntries));
+        }
+
+        public void printHeuristic() {
+            ArrayFunction f = new ArrayFunction(heuristic);
+            System.out.println(f.convertDataToString());
+        }
+    }
+
+    @Test
+    public void testCacheStatsValues() throws IOException {
+
+        TestTools.initUciEngineTest();
+        System.setProperty("jacky.logging.activate", "false");
+        Logging.initLogging();
+//        ConfigValues.getConfigValues().hash.setValue(256);
+        Caching.CACHING.getTtCache().reset();
+        GameContext gamecontext = new GameContext();
+        // now starting engine:
+        Engine engine = new Engine();
+        engine.getBoard().setStartPosition();
+        System.out.println(engine.getBoard().toUniCodeStr());
+        SearchParameter parameter = params(60000, 12);
+        Move move = engine.go(parameter, new GameState(engine.getBoard()), gamecontext);
+
+        System.out.println(move.toStr());
+
+        // with the evaluation function it should yield:
+        assertThat(move.toStr()).isIn("d2d4", "g1f3", "e2e4");
+
+        CacheInfo run1 = new CacheInfo(Caching.CACHING.getTtCache());
+
+        Caching.CACHING.getTtCache().resetStatistics();
+        // redo same "go":
+        parameter = params(60000, 12);
+        move = engine.go(parameter, new GameState(engine.getBoard()), gamecontext);
+
+        CacheInfo run2 = new CacheInfo(Caching.CACHING.getTtCache());
+
+        Caching.CACHING.getTtCache().resetStatistics();
+        // redo same "go":
+        parameter = params(60000, 12);
+        move = engine.go(parameter, new GameState(engine.getBoard()), gamecontext);
+
+        CacheInfo run3 = new CacheInfo(Caching.CACHING.getTtCache());
+
+        Caching.CACHING.getTtCache().resetStatistics();
+        // redo same "go":
+        parameter = params(600000, 15);
+        move = engine.go(parameter, new GameState(engine.getBoard()), gamecontext);
+
+        CacheInfo run4 = new CacheInfo(Caching.CACHING.getTtCache());
+
+        System.out.println("run 1 info:");
+        run1.printInfo();
+        System.out.println("run 2 info:");
+        run2.printInfo();
+        System.out.println("run 3 info:");
+        run3.printInfo();
+        System.out.println("run 4 info:");
+        run3.printInfo();
+
+        run3.printHeuristic();
+    }
+}
