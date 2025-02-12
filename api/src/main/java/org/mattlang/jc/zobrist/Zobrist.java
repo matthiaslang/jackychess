0e7408391b1b8b69b5cc02253a68fb11c5791fdc
lang <mla@mailbox.org>
2025-02-12 11:45:28 +0100

    faster zobrist hash initialisation

--- api/src/main/java/org/mattlang/jc/zobrist/Zobrist.java
+++ api/src/main/java/org/mattlang/jc/zobrist/Zobrist.java
@@ -47,11 +47,14 @@ public final class Zobrist {
 
     public static long hash(BoardRepresentation board) {
         long h = 0;
-        for (int i = 0; i < NUM_BOARD_FIELDS; i++) {
-            byte fig = board.getFigureCode(i);
+        long pieceSet = board.getBoard().getPieces();
+        while (pieceSet != 0) {
+            final int piece = Long.numberOfTrailingZeros(pieceSet);
+            byte fig = board.getFigureCode(piece);
             if (fig != FigureConstants.FT_EMPTY) {
-                h = updateFig(h, i, fig);
+                h = updateFig(h, piece, fig);
             }
+            pieceSet &= pieceSet - 1;
         }
         h = updateEnPassant(h, board.getEnPassantMoveTargetPos());
         h = updateCastling(h, board.getCastlingRights());
@@ -64,11 +67,14 @@ public final class Zobrist {
 
     public static long hashPawnsAndKings(BoardRepresentation board) {
         long h = 0;
-        for (int i = 0; i < NUM_BOARD_FIELDS; i++) {
-            byte fig = board.getFigureCode(i);
+        long pieceSet = board.getBoard().getPieces();
+        while (pieceSet != 0) {
+            final int piece = Long.numberOfTrailingZeros(pieceSet);
+            byte fig = board.getFigureCode(piece);
             if (isKingOrPawn(fig)) {
-                h = updateFig(h, i, fig);
+                h = updateFig(h, piece, fig);
             }
+            pieceSet &= pieceSet - 1;
         }
         return h;
     }
