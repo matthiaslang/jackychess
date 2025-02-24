a32f9aeac88899423f52cf6b52241bc99820a6cf
lang <mla@mailbox.org>
2025-02-24 17:27:54 +0100

    fixed Issue with not removing castling rights when a Rook gets captured

---
+++ engine/src/test/java/org/mattlang/jc/problemanalyzing/CastlingRightsWrongOnCaptureTest.java
@@ -0,0 +1,38 @@
+package org.mattlang.jc.problemanalyzing;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mattlang.jc.chessTests.EpdParsing.emptyPos;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.mattlang.jc.board.*;
+import org.mattlang.jc.engine.Engine;
+import org.mattlang.jc.uci.UCI;
+import org.mattlang.jc.util.Logging;
+import org.mattlang.tuning.data.pgnparser.AlgebraicNotation;
+import org.mattlang.tuning.data.pgnparser.MoveText;
+
+public class CastlingRightsWrongOnCaptureTest {
+
+    @Test
+    public void analyzeCastlingRightsWrongWhenCapturingARook() throws IOException {
+
+        Logging.initLogging();
+        UCI.instance.attachStreams();
+
+        // now starting engine:
+        Engine engine = new Engine();
+        GameState gameState = engine.getBoard().setFenPosition("position fen rnbqrbk1/2pn1ppp/1p2p3/2PpP3/1P1P4/2N1B3/4NPPP/R2QKB1R b KQ - - -");
+        System.out.println(engine.getBoard().toUniCodeStr());
+        BoardRepresentation board=gameState.getBoard();
+
+        Move move = AlgebraicNotation.moveFromAN(board, Color.BLACK, new MoveText("Rxa1", emptyPos()));
+        board.domove(move);
+
+        assertThat(board.isCastlingAllowed(CastlingType.WHITE_LONG)).isFalse();
+        assertThat(board.isCastlingAllowed(CastlingType.WHITE_SHORT)).isTrue();
+
+    }
+
+}
