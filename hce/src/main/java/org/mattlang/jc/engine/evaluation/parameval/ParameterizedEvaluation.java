32d796d1b653e2bcd77c45d92f05507ad963c6fa
lang <mla@mailbox.org>
2025-03-17 13:24:58 +0100

    Revert "removed material correction (obsolete due to complexity and drawByMaterial checks)"
    This reverts commit 29007aeaf48f203660d2065c24b858874759a22f.

--- hce/src/main/java/org/mattlang/jc/engine/evaluation/parameval/ParameterizedEvaluation.java
+++ hce/src/main/java/org/mattlang/jc/engine/evaluation/parameval/ParameterizedEvaluation.java
@@ -11,7 +11,6 @@ import org.mattlang.jc.board.BoardRepresentation;
 import org.mattlang.jc.board.Color;
 import org.mattlang.jc.engine.EvaluateFunction;
 import org.mattlang.jc.engine.TuningCache;
-import org.mattlang.jc.engine.evaluation.Weights;
 import org.mattlang.jc.engine.evaluation.annotation.EvalConfigurable;
 import org.mattlang.jc.engine.evaluation.annotation.EvalConfigurator;
 import org.mattlang.jc.engine.evaluation.parameval.endgame.EndGameRules;
@@ -57,6 +56,8 @@ public class ParameterizedEvaluation implements EvaluateFunction {
     private final ParameterizedComplexityEvaluation complexityEvaluation;
 
     private final ParameterizedSpaceEvaluation spaceEvaluation;
+
+    private final ParameterizedMaterialCorrectionEvaluation matCorrection;
 
     @Getter
     private final ParameterizedAdjustmentsEvaluation adjustments;
@@ -110,6 +111,7 @@ public class ParameterizedEvaluation implements EvaluateFunction {
         mobEvaluation = new ParameterizedMobilityEvaluation();
         pawnEvaluation = new ParameterizedPawnEvaluation(forTuning, caching);
 
+        matCorrection = new ParameterizedMaterialCorrectionEvaluation(config);
         adjustments = new ParameterizedAdjustmentsEvaluation();
 
         threatsEvaluation = new ParameterizedThreatsEvaluation();
@@ -155,10 +157,6 @@ public class ParameterizedEvaluation implements EvaluateFunction {
             if (cachedResult != IntIntCache.NORESULT) {
                 return cachedResult;
             }
-        }
-
-        if (currBoard.getMaterial().isDrawByMaterial()) {
-            return Weights.REPETITION_WEIGHT;
         }
 
         if (forTuning && optimizeMode) {
@@ -208,6 +206,8 @@ public class ParameterizedEvaluation implements EvaluateFunction {
 
         int score = result.calcCompleteScore(currBoard);
 
+        score = matCorrection.correct(currBoard, score);
+
         int who2mov = who2Move == Color.WHITE ? 1 : -1;
         score = score * who2mov;
 
@@ -249,6 +249,8 @@ public class ParameterizedEvaluation implements EvaluateFunction {
         withTuningCaching(SPACE, () -> spaceEvaluation.eval(result, currBoard));
 
         int score = result.calcCompleteScore(currBoard);
+
+        score = matCorrection.correct(currBoard, score);
 
         int who2mov = who2Move == Color.WHITE ? 1 : -1;
         score = score * who2mov;
