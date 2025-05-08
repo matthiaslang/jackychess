064c13ae699a62a15508a74566f649c6b1f6ce76
lang <mla@mailbox.org>
2025-05-08 09:27:30 +0200

    fixed testcase

--- engine/src/test/java/org/mattlang/jc/uci/UciInterfaceTest.java
+++ engine/src/test/java/org/mattlang/jc/uci/UciInterfaceTest.java
@@ -69,13 +69,13 @@ public class UciInterfaceTest {
         communication.write("position startpos moves e2e4 e7e5 g1f3 d7d5");
         communication.write("go depth 1");
         Thread.sleep(2000);
-        communication.expectBestmove("e4d5");
+        communication.expectBestmove("e4d5", "e4d5", "f3e5");
         communication.consumeAllInfo();
 
         communication.write("position startpos moves e2e4 e7e5 g1f3 d7d5");
         communication.write("go nodes 10000");
         Thread.sleep(2000);
-        communication.expectBestmove("e4d5");
+        communication.expectBestmove("e4d5", "e4d5", "f3e5");
         communication.consumeAllInfo();
     }
 
