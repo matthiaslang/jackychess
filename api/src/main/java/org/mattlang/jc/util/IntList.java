3c216028ddf49a54e5b294b7030b7f1c76722d24
lang <mla@mailbox.org>
2025-11-17 16:43:27 +0100

    refactoring: move filtering of moves into staged move iteration preparer code

--- api/src/main/java/org/mattlang/jc/util/IntList.java
+++ api/src/main/java/org/mattlang/jc/util/IntList.java
@@ -1,35 +1,21 @@
 package org.mattlang.jc.util;
 
 /**
- * Simple list class for ints.
+ * Simple list class for ints with fixed maximum size.
  */
 public final class IntList {
 
-    private int[] data;
+    private final int[] data;
 
     private int size = 0;
-
-    public IntList() {
-        this(40);
-    }
 
     public IntList(int maxSize) {
         data = new int[maxSize];
     }
 
     public void add(int move) {
-        if (size < data.length) {
-            data[size] = move;
-            size++;
-        } else {
-            int newSize = data.length + data.length;
-            int[] newmoves = new int[newSize];
-            System.arraycopy(data, 0, newmoves, 0, data.length);
-            data = newmoves;
-
-            data[size] = move;
-            size++;
-        }
+        data[size] = move;
+        size++;
     }
 
     public int size() {
